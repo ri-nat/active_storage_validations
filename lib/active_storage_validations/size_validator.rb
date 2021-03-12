88fe49e0f58c4319dd13473d7ba92e0a93651837
Rinat Shaykhutdinov <rinat@stonedevs.com>
2021-03-13 02:11:17 +0300

    Use 0 as byte_size if blob is `nil`

--- lib/active_storage_validations/size_validator.rb
+++ lib/active_storage_validations/size_validator.rb
@@ -22,9 +22,10 @@ module ActiveStorageValidations
       errors_options[:message] = options[:message] if options[:message].present?
 
       files.each do |file|
-        next if content_size_valid?(file.blob.byte_size)
+        byte_size = file.blob&.byte_size.to_i
+        next if content_size_valid?(byte_size)
 
-        errors_options[:file_size] = number_to_human_size(file.blob.byte_size)
+        errors_options[:file_size] = number_to_human_size(byte_size)
         record.errors.add(attribute, :file_size_out_of_range, **errors_options)
         break
       end
